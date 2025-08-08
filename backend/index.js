af90a3ce26e2da9f5478e0e2ec32597d4f3d306d
Zairth <mano.gimenez@hotmail.com>
2025-08-08 12:32:08 +0200

    Modif de la string de la route '\' d'express

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Railway!");
+  res.send("Hello from Railway! \n L'URL est PUBLIQUE et STATIQUE, elle ne changeera pas même si j'éteint mon PC");
 });
 
 app.listen(PORT, () => {
