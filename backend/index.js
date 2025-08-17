d477e3b3c9ee24eae76e363affd9cea6e6845002
Zairth <mano.gimenez@hotmail.com>
2025-08-17 16:39:11 +0200

    modif du readme

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-import { PrismaClient } from "@prisma/client";
+// backend/index.js
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
 
+// Importer Prisma
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "..", "frontend", "src")));
-app.use(express.json());
-
+app.use(express.json()); // ⚠️ JSON middleware avant les routes POST
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "src", "index.html"));
