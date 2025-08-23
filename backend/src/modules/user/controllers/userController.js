9bd26a756b955ff9a78626539f36aab98304a25d
Zairth <mano.gimenez@hotmail.com>
2025-08-23 16:52:59 +0200

    Routes auth & users terminé (normalement, elles sont assez basiques)

--- backend/src/modules/user/controllers/userController.js
+++ backend/src/modules/user/controllers/userController.js
@@ -81,3 +81,54 @@ exports.getUsers = async (req, res) => {
     res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs.' });
   }
 };
+
+
+// GET /users/:id -> récupérer un utilisateur par son ID
+// Middlewares "verifyToken" et "isHimselfOrAdmin" appelés avant d'arriver ici
+exports.getUserById = async (req, res) => {
+
+  const { id } = req.params;
+
+  try {
+
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: { id: true, username: true, email: true, role: true }
+    });
+    if (!user) return res.status(404).json({ error: "Utilisateur non trouvé." });
+
+    res.json(user);
+
+  } catch (error) {
+
+    console.error("Erreur getUserById:", error);
+    res.status(500).json({ error: "Erreur lors de la récupération de l'utilisateur." });
+  }
+};
+
+
+// DELETE /users/:id -> supprimer un utilisateur par son ID
+// Middlewares "verifyToken" et "isHimselfOrAdmin" appelés avant d'arriver ici
+exports.deleteUserById = async (req, res) => {
+
+  const { id } = req.params;
+
+  try {
+
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) return res.status(404).json({ error: "Utilisateur non trouvé." });
+
+    if (user.role === 'ADMIN') {
+      return res.status(403).json({ error: "Impossible de supprimer cet utilisateur." });
+    }
+
+    await prisma.user.delete({ where: { id } });
+
+    res.json({ message: "Utilisateur supprimé.", user });
+
+  } catch (error) {
+
+    console.error("Erreur deleteUserById:", error);
+    res.status(500).json({ error: "Erreur lors de la suppression de l'utilisateur." });
+  }
+};
--- backend/src/modules/user/routes/userRoutes.js
+++ backend/src/modules/user/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const { register, getUsers, updateUser } = require('../controllers/userController');
+// Controllers
+const { register, getUsers, updateUser, getUserById, deleteUserById } = require('../controllers/userController');
 
 // Middlewares
 const isAdmin = require('../../../middlewares/isAdmin');
@@ -17,8 +18,13 @@ router.post('/', validateRegister, register);
 
 // GET /users -> récupérer tous les users
 router.get('/', verifyToken, isAdmin, getUsers);
+// GET /users/:id -> récupérer un user par son ID
+router.get('/:id', verifyToken, isHimselfOrAdmin, getUserById);
 
 // PUT /users/:id -> mettre à jour un user
 router.put('/:id', verifyToken, isHimselfOrAdmin, validateUpdate, updateUser);
+
+// DELETE /users/:id -> supprimer un user par son ID
+router.get('/:id', verifyToken, isHimselfOrAdmin, deleteUserById);
 
 module.exports = router;
